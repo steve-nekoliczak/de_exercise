e53f7f725e78a2a6e296a278ea5a7997e6e22ccc
steve-nekoliczak <steve.nekoliczak@protonmail.com>
2019-05-27 17:02:10 -0500

    -Adding ex_types param to get_document. -Refactoring how ex_types are kept in the result with keep_ex_types().

--- exercise/api.py
+++ exercise/api.py
@@ -7,11 +7,23 @@ from pymongo import ASCENDING, DESCENDING
 import config
 
 
-def get_document(document_title):
+def keep_ex_types(ex_dict, ex_types):
+    for ex in ex_dict:
+        for j, tw in reversed(list(enumerate(ex['topic_words']))):
+            if tw['type'] not in ex_types:
+                del ex['topic_words'][j]
+    return ex_dict
+
+
+def get_document(document_title, ex_types=[]):
     result = config.mongo.db.exercise.find({'document_title': document_title})
     json_result = loads(dumps(result, json_options=RELAXED_JSON_OPTIONS))
     if len(json_result) == 0:
         return 'Document with title \'' + document_title + '\' was not found.', 404
+
+    if ex_types:
+        json_result = keep_ex_types(json_result, ex_types)
+
     return json_result
 
 
@@ -36,11 +48,8 @@ def get_paragraph(document_title, paragraph_index, ex_types=[]):
         {'document_title': document_title, 'paragraph_index': paragraph_index})
     json_result = loads(dumps(result, json_options=RELAXED_JSON_OPTIONS))
 
-    # Only keep exercise types from ex_types.
-    for ex in json_result:
-        for j, tw in reversed(list(enumerate(ex['topic_words']))):
-            if tw['type'] not in ex_types:
-                del ex['topic_words'][j]
+    if ex_types:
+        json_result = keep_ex_types(json_result, ex_types)
 
     return json_result
 
