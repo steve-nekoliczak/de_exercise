91d21f92b5b8d028624667565b93e18c6460d9b3
steve-nekoliczak <steve.nekoliczak@protonmail.com>
2019-06-23 18:37:04 -0500

    Adding argument for debug. Also removing dummydata option.

--- server.py
+++ server.py
@@ -6,15 +6,16 @@ from exercise.builder import generate_ex
 
 
 def get_args():
-    ap = argparse.ArgumentParser('Process human language sentences into JSON.')
+    ap = argparse.ArgumentParser('Create exercises from German text.')
 
     # Add args
+    ap.add_argument('-d', '--debug',
+                    action='store_true',
+                    help="Enable debugging mode.")
+
     ap.add_argument('-p', '--port', type=int,
                     help="Port number to run this service on.",
                     default=5011)
-
-    ap.add_argument('-d', '--dummydata',
-                    help="Initialize the database with dummy data.")
 
     ap.add_argument('-t', '--text-files-dir',
                     help="Directory to store uploaded text files.",
@@ -30,9 +31,5 @@ if __name__ == "__main__":
 
     text_files_dir = args.text_files_dir
 
-    if args.dummydata:
-        filename = os.path.abspath(os.path.join(os.getcwd(), 'die_stopfnadel.txt'))
-        generate_ex(filename, 'Die Stopfnadel', 'H. C. Andersen')
-
-    connex_app.run(debug=True, port=args.port)
+    connex_app.run(debug=args.debug, port=args.port)
 
